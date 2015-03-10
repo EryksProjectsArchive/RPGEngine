164afa7ba1ab9e5a0b2867f66dfa2107e375e18e
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-03-10 11:48:02 +0100

    Added baking of tile texture

--- src/tools/MapMaker/TileInfo.cs
+++ src/tools/MapMaker/TileInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,35 @@ namespace MapMaker
 {
     class TileInfo
     {
-        private Image image = null;
+        private Bitmap image = null;
         private UInt32 width = 0;
         private UInt32 height = 0;
 
         public TileInfo(Image sourceImage, Rectangle sourceRect, UInt32 width, UInt32 height)
         {
             this.image = null;
-            this.width = 0u;
-            this.height = 0u;
+            this.width = width;
+            this.height = height;
 
             MakeImage(sourceImage, sourceRect, width, height);
         }
 
         private void MakeImage(Image sourceImage, Rectangle sourceRect, UInt32 width, UInt32 height)
         {
-            // TODO
+            if(image != null)
+            {
+                image.Dispose();
+            }
+            image = new Bitmap((int)width, (int)height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            image.SetResolution(sourceImage.HorizontalResolution, sourceImage.VerticalResolution);
+
+            Graphics graphics = Graphics.FromImage(image);
+
+            graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+
+            graphics.Clear(Color.Black);
+            graphics.DrawImage(sourceImage, new Rectangle(0, 0, (int)width, (int)height), sourceRect, GraphicsUnit.Pixel);
+            graphics.Dispose();
         }
 
         public Image GetImage()
